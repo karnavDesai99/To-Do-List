07fa762c0d074f37c318117aece62bd80b26fa44
karnavDesai99 <45823615+karnavDesai99@users.noreply.github.com>
2021-01-11 19:53:46 +0530

    Add files via upload

---
+++ tasks/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+class newTaskForm(forms.Form):
+    task=forms.CharField(label="New task")
+
+def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"]=[]
+    return render(request,"tasks/index.html",{
+        "tasks":request.session["tasks"],
+    })
+
+def add(request):
+    if request.method=="POST":
+        form=newTaskForm(request.POST)
+        if form.is_valid():
+            task=form.cleaned_data["task"]
+            request.session["tasks"]+=[task]
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request,"tasks/add.html",{
+                "form":form
+            })
+    return render(request,"tasks/add.html",{
+        "form":newTaskForm()
+    })
